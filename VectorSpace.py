2e7ef3e5ecae12b644527708988335138f64dd41
Chi-Hung Chang <chihung861224@gmail.com>
2019-04-09 19:35:08 +0800

    annotate in order to test tb

--- VectorSpace.py
+++ VectorSpace.py
@@ -2,6 +2,7 @@ from Parser import Parser
 import util
 import tfidf
 import numpy as np
+from textblob import TextBlob as tb
 
 class VectorSpace:
     """ A algebraic model for representing text documents as vectors of identifiers. 
@@ -12,8 +13,10 @@ class VectorSpace:
     # Collection of document term vectors
     documentVectors = []
 
+    #bloblist = []
+
     # Collection of document feedback vectors
-    feedbackVectors = []
+    #feedbackVectors = []
 
     # Mapping of vector index to keyword
     vectorKeywordIndex=[]
@@ -22,18 +25,21 @@ class VectorSpace:
     parser=None
 
 
-    def __init__(self, documents = [], bloblist = []):
+    def __init__(self, documents = []):
+    #def __init__(self, documents = [], bloblist = []):
         self.documentVectors=[]
         self.parser = Parser()
         if(len(documents)>0):
-            self.build(documents, bloblist)
+            self.build(documents)
+            #self.build(documents, bloblist)
 
-    def build(self,documents, bloblist):
+    def build(self,documents):
+    #def build(self,documents, bloblist):
         """ Create the vector space for the passed document strings """
+        #self.bloblist = [tb(document) for document in documents]
         self.vectorKeywordIndex = self.getVectorKeywordIndex(documents)
         self.documentVectors = [self.makeVector(document) for document in documents]
         #self.tfidfVectors = [self.makeTfidf(document, blob, bloblist) for document, blob in zip(documents, bloblist)]
-        self.tfidfVectors = [self.makeTfidf(document, blob, bloblist) for document, blob in zip(documents, bloblist)]
 
         #print self.vectorKeywordIndex
         #print self.documentVectors
@@ -72,22 +78,22 @@ class VectorSpace:
 
     
     # def makeTfidf(self, wordString, bloblist):
-    def makeTfidf(self, wordString, blob, bloblist):
+    #def makeTfidf(self, wordString, blob, bloblist):
         """ @pre: unique(vectorIndex) """
 
         #Initialise vector with 0's
-        vector = [0] * len(self.vectorKeywordIndex)
-        wordList = self.parser.tokenise(wordString)
-        wordList = self.parser.removeStopWords(wordList)
-        for word in wordList:
-            vector[self.vectorKeywordIndex[word]] = tfidf.tfidf(word, blob, bloblist)
+        #vector = [0] * len(self.vectorKeywordIndex)
+        #wordList = self.parser.tokenise(wordString)
+        #wordList = self.parser.removeStopWords(wordList)
+        #for word in wordList:
+        #    vector[self.vectorKeywordIndex[word]] = tfidf.tfidf(word, blob, bloblist)
             #vector[self.vectorKeywordIndex[word]] = {tfidf.tfidf(word, blob, bloblist) for blob, word in zip(bloblist, blob.words)}
-        return vector
+        #return vector
 
     
-    def makeFeedback(self, count, queryVector):
-        vector = np.array(queryVector) + 0.5 * np.array(documentVectors[count])
-        return vector
+    #def makeFeedback(self, count, queryVector):
+        #vector = np.array(queryVector) + 0.5 * np.array(documentVectors[count])
+        #return vector
     
     
     def buildQueryVector(self, termList):
@@ -96,10 +102,9 @@ class VectorSpace:
         return query
 
 
-    def buildQueryTfidf(self, termList, bloblist):
+    #def buildQueryTfidf(self, termList, bloblist):
         #query = self.makeTfidf((" ".join(termList), blob, bloblist) for blob in bloblist)
-        query = self.makeTfidf((" ".join(termList), blob, bloblist) for blob in bloblist)
-        return query
+        #return query
 
 
     def related(self,documentId):
@@ -127,31 +132,31 @@ class VectorSpace:
         return ratings
     
     
-    def searchTfidfByCosine(self, searchList, bloblist):
-        """ search for documents that match based on a list of terms """
-        queryVector = self.buildQueryTfidf(searchList, bloblist)
+    #def searchTfidfByCosine(self, searchList, bloblist):
+        #""" search for documents that match based on a list of terms """
+        #queryVector = self.buildQueryTfidf(searchList, bloblist)
 
-        ratings = [util.cosine(queryVector, documentVector) for documentVector in self.documentVectors]
+        #ratings = [util.cosine(queryVector, documentVector) for documentVector in self.documentVectors]
         #ratings.sort(reverse=True)
-        return ratings
+        #return ratings
     
     
-    def searchTfidfByEuclidean(self, searchList, bloblist):
-        """ search for documents that match based on a list of terms """
-        queryVector = self.buildTfidf(searchList, bloblist)
+    #def searchTfidfByEuclidean(self, searchList, bloblist):
+        #""" search for documents that match based on a list of terms """
+        #queryVector = self.buildTfidf(searchList, bloblist)
 
-        ratings = [util.euclidean(queryVector, documentVector) for documentVector in self.documentVectors]
+        #ratings = [util.euclidean(queryVector, documentVector) for documentVector in self.documentVectors]
         #ratings.sort(reverse=True)
-        return ratings
+        #return ratings
 
 
-    def searchFeedback(self, searchList, bloblist):
-        queryVector = self.buildQueryVector(searchList)
+    #def searchFeedback(self, searchList, bloblist):
+        #queryVector = self.buildQueryVector(searchList)
 
-        feedbackVectors = [self.makeFeedback(count, queryVector) for count in range(len(documentVectors))]
+        #feedbackVectors = [self.makeFeedback(count, queryVector) for count in range(len(documentVectors))]
 
-        feedbackTfidfVectors = self.buildQueryTfidf(searchList, bloblist)
+        #feedbackTfidfVectors = self.buildQueryTfidf(searchList, bloblist)
 
-        ratings = [util.cosine(feedbackTfidfVectors, documentVector) for documentVector in self.documentVectors]
-        return ratings
+        #ratings = [util.cosine(feedbackTfidfVectors, documentVector) for documentVector in self.documentVectors]
+        #return ratings
 
